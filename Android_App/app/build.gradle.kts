7ef1adb4401b83c1dc440f8303344e6db7c2e7a2
AndyLi <yungyu405728@gmail.com>
2026-05-30 14:22:52 +0800

    chore: bump version to 1.1.3

--- Android_App/app/build.gradle.kts
+++ Android_App/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.andy.macdock"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.2"
+        versionCode = 3
+        versionName = "1.1.3"
     }
 
     buildTypes {
