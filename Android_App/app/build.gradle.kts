1d8e60f819bb007c396fa3802d7dc84502d8e59b
AndyLi <yungyu405728@gmail.com>
2026-05-30 12:15:58 +0800

    Bump version to 1.1.2

--- Android_App/app/build.gradle.kts
+++ Android_App/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.andy.macdock"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.2"
     }
 
     buildTypes {
